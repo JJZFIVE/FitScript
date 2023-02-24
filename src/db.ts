f56e065e584e8b8a6292014004cd2d88fc660523
Joe Zakielarz <jjzfive@gmail.com>
2023-02-24 14:38:02 -0500

    Add docker IP address for connection

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,10 @@
 const { Pool } = require("pg");
 
+const POSTGRES_DOCKER_IP = "172.17.0.3";
+
 const pool = new Pool({
   max: 20,
-  connectionString: "postgres://postgres@localhost:5432/postgres",
+  connectionString: `postgres://postgres@${POSTGRES_DOCKER_IP}:5432/postgres`,
   idleTimeoutMillis: 30000,
 });
 
