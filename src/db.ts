c55bdd14d9a4f350a37ab01f8cbad442ee731657
Joe Zakielarz <jjzfive@gmail.com>
2023-02-24 14:49:10 -0500

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -2,9 +2,15 @@ const { Pool } = require("pg");
 
 const POSTGRES_DOCKER_IP = "172.17.0.3";
 
+const PORT = 5432;
+const DATABASE = "postgres";
+const USER = "postgres";
+// Note: password cannot be null or else there's a docker connection issue
+let PASSWORD = "fitscriptpassword";
+
 const pool = new Pool({
   max: 20,
-  connectionString: `postgres://postgres@${POSTGRES_DOCKER_IP}:5432/postgres`,
+  connectionString: `postgres://${DATABASE}:${PASSWORD}@${POSTGRES_DOCKER_IP}:${PORT}/${USER}`,
   idleTimeoutMillis: 30000,
 });
 
