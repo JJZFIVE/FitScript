af2a9363e38140ccbef0a26a7bfc1aef2bb7847a
Joe Zakielarz <jjzfive@gmail.com>
2023-03-02 18:37:45 -0500

    Upgrade to ChatGPT

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,14 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
-const POSTGRES_DOCKER_IP = "172.17.0.2";
+const POSTGRES_DOCKER_IP =
+  process.env.NODE_ENV == "dev" ? "localhost" : "172.17.0.2";
 
 const PORT = 5432;
 const DATABASE = "postgres";
 const USER = "postgres";
 // Note: password cannot be null or else there's a docker connection issue
-let PASSWORD = "fitscriptpassword";
+const PASSWORD = process.env.NODE_ENV == "dev" ? "" : "fitscriptpassword";
 
 const pool = new Pool({
   max: 20,
---
+++ testing/turbo.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function handler() {
+  try {
+    let MESSAGE =
+      "Help me make a workout plan for a bigger chest" +
+      " Do not include weights yet. (END OF PROMPT)";
+
+    // MESSAGE = "Who are you?";
+    MESSAGE = "How do I build muscle?";
+
+    const firstname = "Joe";
+
+    const messages = [
+      {
+        role: "system",
+        content: `You are FitScript, a helpful personal trainer that lives in SMS texts. Be semi concise. The user's name is ${firstname}, do not overuse their name.`,
+      },
+      { role: "user", content: MESSAGE },
+    ];
+
+    const role = "assistant";
+    const gptResponse = await openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+        // temperature: 0.5,
+        // max_tokens: 200,
+        // top_p: 1,
+        // frequency_penalty: 0,
+        // presence_penalty: 0,
+      })
+      .then((response) => response.data.choices[0].message.content.trim());
+
+    console.log(gptResponse);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+handler();
